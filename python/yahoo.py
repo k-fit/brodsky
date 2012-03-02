f0e71b6c22406f07f337a8569901d804f28dcee0
Matt <matthew.deland@gmail.com>
2012-03-02 07:45:51 -0500

    bugs fixed

--- python/yahoo.py
+++ python/yahoo.py
@@ -7,14 +7,20 @@ url = 'http://www.cnn.com/2011/11/11/world/europe/greece-main/index.html'
 key = keys.ydn_key
 
 def getYahooContent(u):
+    key = keys.ydn_key
     querylang = "http://query.yahooapis.com/v1/public/yql?q=select * from contentanalysis.analyze where url=\'"+u +"\';&diagnostics=true&format=json&" + key
-    
-    jresult = eval(urllib.urlopen(querylang).read())
-    
-    categories = jresult['query']['results']['yctCategories']['yctCategory']
-    entities = jresult['query']['results']['entities']['entity']
-    
-    elist = []
-    for e in entities:
-        elist.append( {'score' : e['score'], 'text' : e['text']['content'] } )
-    return ({'categories' : categories, 'entities' : elist})
+
+    try:
+        jresult = eval(urllib.urlopen(querylang).read())
+        
+        categories = jresult['query']['results']['yctCategories']['yctCategory']
+        entities = jresult['query']['results']['entities']['entity']
+        
+        elist = []
+        for e in entities:
+            elist.append( {'score' : e['score'], 'text' : e['text']['content'] } )
+            return ({'categories' : categories, 'entities' : elist})
+        
+    except:
+        return({'categories' : [], 'entities' : []})
+

18ef1b8308570096aacdc23e09440acf34b3f97a
Matt <matthew.deland@gmail.com>
2012-03-04 12:29:45 -0500

    allow multiple calls to yahoo per url

--- python/topsy_fun.py
+++ python/topsy_fun.py
@@ -6,7 +6,7 @@ import re
 from unidecode import unidecode
 import ast
 import pprint
-
+import time
 import keys
 from gen_sent_fun import *
 from yahoo import *
@@ -21,12 +21,12 @@ def topsy(time_unit = 'day', src = 'all_internet', call = 'top100', p = 0, debug
             print "you didn't change this yet"
             sys.exit(1)
         important_data = []
-        important_data += r.response.list.o # for testing, just take the first 10
-	#while (len(r.response.list.o)) :
-        #    important_data += r.response.list.o
-        #    r.next_page()
-	#    print len(important_data)
-	#print "done"
+        #important_data += r.response.list.o # for testing, just take the first 10
+	while (len(r.response.list.o)) :
+            important_data += r.response.list.o
+            r.next_page()
+	    print len(important_data)
+	print "done"
         
         for i in range(len(important_data)):  
         # call topsy histogram api, record histogram
@@ -37,8 +37,13 @@ def topsy(time_unit = 'day', src = 'all_internet', call = 'top100', p = 0, debug
             print title, url
 	    h(q = title, slice = 3600, period = 24 * 30)
             important_data[i]['histogram'] = h.response.o['histogram']
-            important_data[i]['yahoo'] = getYahooContent(url, debug)
-            important_data[i]['gen_sent'] = []
+            attempt = 0
+	    important_data[i]['yahoo'] = getYahooContent(url, debug)
+            while important_data[i]['yahoo']['categories'] == [] and attempt < 3:
+		time.sleep(1)
+		attempt += 1
+		important_data[i]['yahoo'] = getYahooContent(url, debug)
+	    important_data[i]['gen_sent'] = []
 
             for k in range(len(important_data[i]['yahoo']['entities'])) : 
                 important_data[i]['gen_sent'].append (gen_sent(important_data[i]['yahoo']['entities'][k]['text'], start_date='20120215', end_date = '20120301', src = src, time_unit = time_unit))
